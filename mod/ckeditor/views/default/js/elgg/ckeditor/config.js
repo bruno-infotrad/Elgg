12ce5aae0ff2f73f7990613d391c0cdc20426168
Bruno <ti@infotrad.ca>
2016-09-11 10:20:40 -0400

    Change default ckeditor height

--- mod/ckeditor/views/default/js/elgg/ckeditor/config.js
+++ mod/ckeditor/views/default/js/elgg/ckeditor/config.js
@@ -17,6 +17,7 @@ define(function(require) {
 		disableNativeSpellChecker: false,
 		disableNativeTableHandles: false,
 		removeDialogTabs: 'image:advanced;image:Link;link:advanced;link:target',
-		autoGrow_maxHeight: $(window).height() - 100
+		autoGrow_maxHeight: $(window).height() - 100,
+		height: 100
 	};
 });
